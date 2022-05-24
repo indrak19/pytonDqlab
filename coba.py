a4347b3685afc8993323f39f2f0c69b2699716ee
kindra19 <96459383+indrak19@users.noreply.github.com>
2022-05-24 19:52:41 +0700

    Add files via upload

---
+++ coba.py
@@ -0,0 +1,16 @@
+angka=1
+while angka <= 75:
+    print(angka)
+    angka +=1
+print('finish') 
+
+data=input('masukkan nilai=')
+
+while int(data) <= 40:
+    if int(data) %2==0:
+        print('genap',data)
+    else:
+        print('ganjil',data)
+int(data)+5
+     
+
